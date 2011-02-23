1e17b4b10b8df14a19f69a4e872555d9484d714c
sam <sam.c.day@gmail.com>
2011-02-23 16:52:02 +1000

    Playing around with profiler.

--- benchmark/read_records.js
+++ benchmark/read_records.js
@@ -1,6 +1,7 @@
 var streamBuffers = require("stream-buffers"),
 	fastcgi = require("../lib"),
-	DuplexStream = require("duplex-stream");
+	DuplexStream = require("duplex-stream"),
+	profiler = require("profiler");
 
 var runTest = function(constructor, num) {
 	var testRecord = constructor();
@@ -10,7 +11,9 @@ var runTest = function(constructor, num) {
 	var writeStream = new streamBuffers.WritableStreamBuffer({
 	});
 	
-	var readStream = new streamBuffers.ReadableStreamBuffer();
+	var readStream = new streamBuffers.ReadableStreamBuffer({
+		chunkSize: num * (testRecord.getSize() + 8)
+	});
 	
 	var fastcgiStream = new fastcgi.FastCGIStream(new DuplexStream(readStream, writeStream));
 	
@@ -20,25 +23,28 @@ var runTest = function(constructor, num) {
 	
 	console.log('starting');
 	var running = true;
-	var num = 0;
+	var done = 0;
 	
 	readStream.pause();
 	readStream.put(writeStream.getContents());
-	
+
+	profiler.resume(profiler.CPU, profiler.HEAP_STATS, profiler.HEAP_SNAPSHOT, profiler.JS_CONSTRUCTORS);
 	fastcgiStream.on("record", function() {
-		if(running) num++;
+		if(running) done++;
+		if(done == num) return;
 	});
 	
 	setTimeout(function() {
 		running = false;
 		
-		console.log("Read " + num + " records.");
+		//profiler.pause();
+		console.log("Read " + done + " records.");
 		readStream.pause();
 	}, 1000);
 
 	readStream.resume();
 };
 
-//runTest(function() { return new fastcgi.records.BeginRequest(Math.floor(Math.random()*300000 + 1), Math.floor(Math.random()*255 + 1)); }, 20000);
-var myParams = [["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"]];
-runTest(function() { return new fastcgi.records.Params(myParams); }, 10000);
+runTest(function() { return new fastcgi.records.BeginRequest(112312313, 123) }, 1000);
+//var myParams = [["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"], ["LOLOLOLOLOL", "HAHAHAHA"]];
+//runTest(function() { return new fastcgi.records.Params(myParams); }, 10000);
